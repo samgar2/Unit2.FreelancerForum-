be4aee8bd96ad4379ed18947d809f7d8161f1aca
Samantha Garabedian <samanthagarabedian@samanthsonalair.lan>
2023-11-27 14:49:20 -0800

    workshop solutions

--- index.js
+++ index.js
@@ -18,26 +18,64 @@ const freelancers = [
         startingPrice: 50
     },
     {
-        name: "The average starting price is $40.",
-       
-    },
-
-    // a few seconds later:
-    {
         name: "Carol",
         occupation: "Programmer",
         startingPrice: 70
     },
-    {
-        name: "The average starting price is $50.",
-       
-    }
 ]
 // END STATE VARIABLES
 
 // Set up interval
-// `setInterval` will call `addFreelancer` every 1000 milliseconds (1 second) and return an interval ID that we can use to stop the interval later. 
-// Calling `clearInterval(addFreelancerIntervalId)` will stop the interval
-const addFreelancerIntervalId = setInterval(addFreelancer, 1000);
+setInterval(addFreelancer, 1000)
 
 render(); // We call this function once to render the initial state
+
+//RENDER function - update to DOM to reflect the current state of the program
+function render() {
+    //Point to existing container in HTML
+    const professionalsContainer = document.querySelector("#professionals")
+    console.log(professionalsContainer)
+
+    //Creating the elements that we want to add
+    const professionalElements = freelancers.map((freelancer) => {
+        const element = document.createElement("li") //Creating <li></li>
+        element.classList.add(freelancer.name, freelancer.occupation, freelancer.startingPrice) //Adding classes to li element
+        element.textContent = `${freelancer.name}, ${freelancer.occupation}, ${freelancer.startingPrice}`; //Make elements show up as text
+        console.log(element)
+        return element //for example: <li class="yellow small"></li>
+        //it will do this for each element in the map
+    })
+
+    //Actually add those created elements to the container
+    professionalsContainer.replaceChildren(...professionalElements)
+}
+
+// Calculate average starting price of my freelancers array
+function addAveragePrice() {
+    let average = 1
+    for (let i = 0; i < startingPrices.length; i++) {
+      average = average + startingPrices[i]/3
+    return average
+    } 
+    const averagePrice = document.querySelector("average");
+    averagePrice.textContent = `The average starting price is ${average}`
+
+    render()
+}
+
+// Add a random freelancer to the `freelancers` array
+function addFreelancer() {
+    const selectedName = names[Math.floor(Math.random() * names.length)]
+    console.log(selectedName)
+
+    const selectedOccupation = occupations[Math.floor(Math.random() * occupations.length)]
+    console.log(selectedOccupation)
+
+    const selectedstartingPrice = startingPrices[Math.floor(Math.random() * startingPrices.length)]
+    console.log(selectedstartingPrice)
+
+    freelancers.push({name: selectedName, occupation: selectedOccupation, startingPrice: selectedstartingPrice})
+
+    render()
+}
+
